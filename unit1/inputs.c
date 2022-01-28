8956f38fa8a5d5f21b4b7457afbb826bafebe8b7
YahirProgrammer <2109145@upy.edu.mx>
2022-01-28 11:28:52 -0600

    confirm

---
+++ unit1/inputs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int num1; //Declarando mi variable
+int num2; 
+
+int main(){
+    //Imprimir mensaje//
+    printf("Enter number1,\n\tnum1: ");
+    //* Inputs *//
+    scanf("%d", &num1); //Inputs Keyboard
+    printf("Enter number2,\n\tnum2: "); //Output Screen
+    scanf("%d", &num2);
+    //*Procesos*//
+    int sum = num1 + num2;
+    printf(" suma = %d\n", sum);
+    return 0;
+}
